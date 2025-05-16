0c3424abeee1e0217ad1b2dd6b3cc5cd82ec0350
Adrian PK <adrianpk@adrianpk.com>
2025-05-16 20:57:17 +0200

    Implement user management

--- test/pulap/accounts_test.exs
+++ test/pulap/accounts_test.exs
@@ -505,4 +505,62 @@ defmodule Pulap.AccountsTest do
       refute inspect(%User{password: "123456"}) =~ "password: \"123456\""
     end
   end
+
+  describe "users" do
+    alias Pulap.Accounts.User
+
+    import Pulap.AccountsFixtures
+
+    @invalid_attrs %{email: nil, hashed_password: nil, confirmed_at: nil}
+
+    test "list_users/0 returns all users" do
+      user = user_fixture()
+      assert Accounts.list_users() == [user]
+    end
+
+    test "get_user!/1 returns the user with given id" do
+      user = user_fixture()
+      assert Accounts.get_user!(user.id) == user
+    end
+
+    test "create_user/1 with valid data creates a user" do
+      valid_attrs = %{email: "some email", hashed_password: "some hashed_password", confirmed_at: ~U[2025-05-15 18:05:00Z]}
+
+      assert {:ok, %User{} = user} = Accounts.create_user(valid_attrs)
+      assert user.email == "some email"
+      assert user.hashed_password == "some hashed_password"
+      assert user.confirmed_at == ~U[2025-05-15 18:05:00Z]
+    end
+
+    test "create_user/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Accounts.create_user(@invalid_attrs)
+    end
+
+    test "update_user/2 with valid data updates the user" do
+      user = user_fixture()
+      update_attrs = %{email: "some updated email", hashed_password: "some updated hashed_password", confirmed_at: ~U[2025-05-16 18:05:00Z]}
+
+      assert {:ok, %User{} = user} = Accounts.update_user(user, update_attrs)
+      assert user.email == "some updated email"
+      assert user.hashed_password == "some updated hashed_password"
+      assert user.confirmed_at == ~U[2025-05-16 18:05:00Z]
+    end
+
+    test "update_user/2 with invalid data returns error changeset" do
+      user = user_fixture()
+      assert {:error, %Ecto.Changeset{}} = Accounts.update_user(user, @invalid_attrs)
+      assert user == Accounts.get_user!(user.id)
+    end
+
+    test "delete_user/1 deletes the user" do
+      user = user_fixture()
+      assert {:ok, %User{}} = Accounts.delete_user(user)
+      assert_raise Ecto.NoResultsError, fn -> Accounts.get_user!(user.id) end
+    end
+
+    test "change_user/1 returns a user changeset" do
+      user = user_fixture()
+      assert %Ecto.Changeset{} = Accounts.change_user(user)
+    end
+  end
 end
