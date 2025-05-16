6962ea7c92de00fee56d0bdeed85a19790ca9417
Adrian PK <adrianpk@adrianpk.com>
2025-05-16 16:24:02 +0200

    Normalize UI styling

--- lib/pulap/auth/permission.ex
+++ lib/pulap/auth/permission.ex
@@ -18,6 +18,25 @@ defmodule Pulap.Auth.Permission do
   def changeset(permission, attrs) do
     permission
     |> cast(attrs, [:slug, :name, :description])
+    |> put_slug()
     |> validate_required([:slug, :name, :description])
+    |> unique_constraint(:slug)
+  end
+
+  # Generate a slug from the name and a UUID segment, like in Role
+  defp put_slug(changeset) do
+    if name = get_field(changeset, :name) do
+      uuid = Ecto.UUID.generate()
+      last_segment = uuid |> String.split("-") |> List.last()
+      slug =
+        name
+        |> String.downcase()
+        |> String.replace(~r/[^a-z0-9]+/u, "-")
+        |> String.trim("-")
+        |> Kernel.<>("-" <> last_segment)
+      change(changeset, slug: slug)
+    else
+      changeset
+    end
   end
 end
