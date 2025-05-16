6790854010abe6fced7a7f1d76415c7b1709eace
Adrian PK <adrianpk@adrianpk.com>
2025-05-17 01:32:01 +0200

    Normalize migrations

--- priv/repo/migrations/20250516215948_create_role_permissions.exs
+++ priv/repo/migrations/20250516215948_create_role_permissions.exs
@@ -5,6 +5,8 @@ defmodule Pulap.Repo.Migrations.CreateRolePermissions do
     create table(:role_permissions, primary_key: false) do
       add :role_id, references(:roles, type: :binary_id, on_delete: :delete_all), null: false
       add :permission_id, references(:permissions, type: :binary_id, on_delete: :delete_all), null: false
+      add :created_by, :binary_id
+      add :updated_by, :binary_id
     end
 
     create unique_index(:role_permissions, [:role_id, :permission_id])
