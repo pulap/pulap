a8bbeb3dbe07bf7f1c07c1550fd264ba43f07144
Adrian PK <adrianpk@adrianpk.com>
2025-05-24 12:31:11 +0200

    Implement address

---
+++ priv/repo/migrations/20250524094502_create_addresses.exs
@@ -0,0 +1,31 @@
+defmodule Pulap.Repo.Migrations.CreateAddresses do
+  use Ecto.Migration
+
+  def change do
+    create table(:addresses, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :slug, :string
+      add :name, :string
+      add :street, :string
+      add :number, :string
+      add :floor, :string
+      add :apartment, :string
+      add :city, :string
+      add :state, :string
+      add :postal_code, :string
+      add :country, :string
+      add :admin_level_0, :string
+      add :admin_level_1, :string
+      add :admin_level_2, :string
+      add :admin_level_3, :string
+      add :admin_level_4, :string
+      add :location_lat, :float
+      add :location_lng, :float
+      add :geohash, :string
+      add :created_by, :uuid
+      add :updated_by, :uuid
+
+      timestamps(type: :utc_datetime)
+    end
+  end
+end
